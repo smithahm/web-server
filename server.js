4e7339e4c3e42689d0b1124aeb0115c17d965892
Smitha Halappa Mallikarjunappa <hmsmitha17@gmail.com>
2016-07-08 11:37:51 -0700

    Initialize repo

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+//capital letter means constant in Javascript
+var PORT = 3000;
+var middleWare = {
+  requireAuthentication: function(req, res, next){
+     console.log(req.method);
+  },
+  logg: function(req, res, next){
+
+    console.log(new Date().toString() + req.method + ' ' + req.originalUrl);
+    next();
+  }
+};
+
+
+app.get('/about',middleWare.logg, function(req, res){
+   res.send('About us');
+});
+
+app.use(express.static(__dirname + '/public'));
+app.listen(PORT, function(){
+	console.log('express server started' + PORT);
+});
